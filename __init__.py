024b7b626fbd4f9edf9e47c9b9096fcbfd893a21
Patrick Beeson <patrickbeeson@gmail.com>
2014-10-09 15:39:38 -0400

    Try for the name first.

--- __init__.py
+++ __init__.py
@@ -15,23 +15,24 @@ def hello():
     if body == '#bff':
         resp.message('#awesome')
         resp.message("What's your name?")
+        user_name = request.values.get('Body', None)
+        if user_name:
+            resp.message("It's great to meet you {name}!".format(name=user_name))
     else:
         resp.message("#lame")
+        break
 
-    user_name = request.values.get('Body', None)
+    # resp.message("Wanna set up a play date?")
 
-    resp.message("It's great to meet you {name}!".format(name=user_name))
-    resp.message("Wanna set up a play date?")
-
-    play_date_response = request.values.get('Body', None)
-    if play_date_response.lower() == 'yes':
-        resp.message("That's great! "
-                     "Get your parent's permission, "
-                     "and go to http://chatsters.com.")
-    elif play_date_response.lower() == 'no':
-        resp.message("Too bad. Maybe another time!")
-    else:
-        resp.message('#lame')
+    # play_date_response = request.values.get('Body', None)
+    # if play_date_response.lower() == 'yes':
+    #     resp.message("That's great! "
+    #                  "Get your parent's permission, "
+    #                  "and go to http://chatsters.com.")
+    # elif play_date_response.lower() == 'no':
+    #     resp.message("Too bad. Maybe another time!")
+    # else:
+    #     resp.message('#lame')
 
     return str(resp)
 
