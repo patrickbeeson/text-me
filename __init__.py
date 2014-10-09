1d66a1e02cb5b774bc39f65b91d512ab88e2c2da
Patrick Beeson <patrickbeeson@gmail.com>
2014-10-09 15:01:57 -0400

    Add index.py and adjust init for logic based on message.

--- __init__.py
+++ __init__.py
@@ -3,13 +3,22 @@ import twilio.twiml
 
 app = Flask(__name__)
 
+responses = {
+    ''
+}
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
     """ Respond to incoming calls with a SMS """
 
     resp = twilio.twiml.Response()
-    resp.message("Hello! Thanks for texting me!")
+
+    if body == '#bff':
+        resp.message('#awesome')
+        resp.message("What's your name?")
+    else:
+        resp.message('#lame')
+
     return str(resp)
 
 if __name__ == "__main__":
