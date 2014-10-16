c47cfbe2d5f7728256f897a4e83561ece35412f4
Patrick Beeson <patrickbeeson@gmail.com>
2014-10-16 16:06:08 -0400

    More refactoring for config.

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import logging
+from logging.handlers import RotatingFileHandler, SMTPHandler
 from flask import Flask
 import os
 
@@ -5,7 +7,35 @@ app = Flask(__name__)
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 
-from app import views
+from . import views
+
+ADMINS = app.config['ADMINS']
+MAIL_SERVER = app.config['MAIL_SERVER']
+MAIL_USERNAME = app.config['MAIL_USERNAME']
+MAIL_PASSWORD = app.config['MAIL_PASSWORD']
+
+credentials = (
+    MAIL_USERNAME,
+    MAIL_PASSWORD
+)
+
+if not app.debug:
+    file_handler = RotatingFileHandler(
+        'textme_errors.log',
+        maxBytes=10000,
+        backupCount=5
+    )
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    mail_handler = SMTPHandler(
+        MAIL_SERVER,
+        'pbeeson@thevariable.com',
+        ADMINS,
+        'An error has occured',
+        credentials=credentials
+    )
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
 
 if __name__ == '__main__':
     app.run()
